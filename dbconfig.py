8bf4b8f31116f1cd9e29c560ce3c563915f8452f
Ani Desktop <anirudhchundawat97@gmail.com>
2023-02-10 15:52:12 +0530

    betfair test 3

--- dbconfig.py
+++ dbconfig.py
@@ -1,4 +1,17 @@
 
+sql_dbname = 'playox'
+sql_host = '172.31.10.241'
+sql_port = '5432'
+sql_user = 'read_write'
+# sql_password =  os.getenv('DB_PASSWORD')
+sql_password = 'X9JLjXklS8ZHarL268JOKFTM'
+
+sql_host1 = '172.31.37.108'
+sql_port1 = '5432'
+sql_user1 = 'postgres_staging'
+sql_password1 = 'UIb0FHxlsrUWe82d7gin'
+sql_dbname1 = 'playox'
+
 sql_dbname_viz = 'playox'
 sql_host_viz = 'analytics.cluster-csf4le3jblm7.ap-south-1.rds.amazonaws.com'
 sql_port_viz = '5432'
