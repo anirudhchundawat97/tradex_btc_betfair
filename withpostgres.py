3973a1f7fd1d9713102aeeb18b8d1b5ba6798e70
Ani Desktop <anirudhchundawat97@gmail.com>
2023-02-14 18:50:27 +0530

    postgres test

--- withpostgres.py
+++ withpostgres.py
@@ -1,4 +1,4 @@
-import psycopg2-binary as psycopg2
+import psycopg2 as psycopg2
 import dbconfig as config
 import logging
 
@@ -14,6 +14,21 @@ class Postgres1:
         logging.info(f"{self.connection}")
         print("before curser")
         self.cursor = self.connection.cursor()
+
+    def insert2_into_db(self, data_dict):
+        # insert data into DB
+        postgres_insert_query = '''insert into test_mm_betfair (
+                                                datetime,
+                                                eventid
+                                                )
+                                                VALUES (%s,%s);'''
+        record_to_insert = (str(data_dict["datetime"]),
+                            str(data_dict["eventid"]),
+                            )
+        print("before execute")
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+        print("before commit")
+        self.connection.commit()
 
     def insert_into_db(self, data_dict):
         # insert data into DB
@@ -51,16 +66,19 @@ class Postgres1:
 
 if __name__ == "__main__":
     db = Postgres1()
+    # data_dict = {"datetime": 12345,
+    #              "eventid": 213453,
+    #              "compid": 74635,
+    #              "eventname": "sher v cheetah",
+    #              "compname": "jungle premier league",
+    #              "team1name": "sher",
+    #              "team2name": "cheetah",
+    #              "team1back": [{"lay":[1.23, 1.26]}],
+    #              "team1lay": [{"lay":[1.23, 1.26]}],
+    #              "team2back": [{"lay":[1.23, 1.26]}],
+    #              "team2lay": [{"lay":[1.23, 1.26]}],
+    #              "inplay": True}
+    # db.insert_into_db(data_dict)
     data_dict = {"datetime": 12345,
-                 "eventid": 213453,
-                 "compid": 74635,
-                 "eventname": "sher v cheetah",
-                 "compname": "jungle premier league",
-                 "team1name": "sher",
-                 "team2name": "cheetah",
-                 "team1back": [{"lay":[1.23, 1.26]}],
-                 "team1lay": [{"lay":[1.23, 1.26]}],
-                 "team2back": [{"lay":[1.23, 1.26]}],
-                 "team2lay": [{"lay":[1.23, 1.26]}],
-                 "inplay": True}
+                 "eventid": 213453}
     db.insert_into_db(data_dict)
