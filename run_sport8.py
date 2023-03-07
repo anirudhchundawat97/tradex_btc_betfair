d184fec415f81ac459c81c414e6165ea37ad0ebe
Ani Desktop <anirudhchundawat97@gmail.com>
2023-03-07 19:36:38 +0530

    strat8 with sports 1.46

--- run_sport8.py
+++ run_sport8.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     print("new 1.02")
     # coin_name = "custom"
     custom_phrase_match = "ISL: Mumbai City FC to win against Bengaluru FC"
-    sport_id = 4
+    sport_id = 1
     while True:
         time_now = dt.datetime.now().time()
         if strat_num == 8:
