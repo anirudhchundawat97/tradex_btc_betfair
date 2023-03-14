4b489104f093d53f53ec366e054a647b04ac3f73
asc_mac <anirudhchundawat97@gmail.com>
2023-03-14 15:27:29 +0530

    strat8 time2expi fix 1.3

--- strat8.py
+++ strat8.py
@@ -107,7 +107,7 @@ class Strategy:
     # upgraded to codes_3/utils/set_finance_timeexpiry_category
     def __set_expiry_category(self):
         now = dt.datetime.now()
-        end = dt.datetime.fromisoformat(self.priceatri.ends_at) + dt.timedelta(hours=5, minutes=30)
+        end = dt.datetime.fromisoformat(self.priceatri.ends_at) - dt.timedelta(hours=5, minutes=30)
         delta = end - now
         print("expiry category:", ">"*25, delta)
         if delta <= dt.timedelta(minutes=1):
